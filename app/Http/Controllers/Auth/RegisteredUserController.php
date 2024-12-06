e81881a41800e1436d979f7723980efabdfbe281
PradipKhanal22 <khanalpradip74@gmail.com>
2024-12-06 11:19:07 +0545

    emaildone

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
 
@@ -44,7 +45,12 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
-
+        // $emaildata = [
+        //     'name' => user->name,
+        // ];
+        // Mail::send('emails.welcomeEmail', $emaildata, function ($message) use () {
+        //     $message->to(user->email, user->name)->subject('Order Notification');
+        // });
         return redirect(route('dashboard', absolute: false));
     }
 }
---
+++ resources/views/emails/welcomeEmail.blade.php
@@ -0,0 +1,7 @@
+<img src="{{asset('logo.png')}}" alt="" class="w-12 h-12">
+<h1>Dear {{$name}},</h1>
+<p>You are Successfully registered to Our Site</p>
+<p>Thank you for choosing us</p>
+<br>
+<p>Regards,</p>
+<h2 class="text-red-600">RetroKits Nepal Family</h2>
