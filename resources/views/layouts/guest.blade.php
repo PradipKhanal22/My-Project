aeae602cd64953b1339a25e7d88bda2baa7c88d4
PradipKhanal22 <khanalpradip74@gmail.com>
2025-07-16 19:22:18 +0545

    streak maintain

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -24,12 +24,9 @@
             </a>
         </div>
 
-
-        
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
             {{ $slot }}
         </div>
     </div>
 </body>
-
 </html>
